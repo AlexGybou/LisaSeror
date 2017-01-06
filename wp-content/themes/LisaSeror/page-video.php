5552c5765813fda094210f0fb975f7d2be97157d
Alexandre Payen <alexandrepayen@yahoo.fr>
2017-01-06 14:03:40 +0100

    des bailles

--- wp-content/themes/LisaSeror/page-video.php
+++ wp-content/themes/LisaSeror/page-video.php
@@ -1,9 +1,11 @@
 <?php get_header(); ?>
 <div class="container">
     <div class="row">
-        <?php $loop = new WP_Query( array( 'post_type' => 'videos', 'posts_per_page' => '10' ) ); ?>
+        <?php $loop = new WP_Query( array( 'post_type' => 'videos', 'posts_per_page' => '2' ) ); ?>
         <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
             <div class="col-md-6">
+                <pre>
+                <?php var_dump($loop); echo '</pre>'; die; ?>
                 <h1><?php the_title() ?></h1>
             </div>
             <div class="col-md-6">
